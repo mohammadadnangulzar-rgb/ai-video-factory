f65cefbfb8d29526b9654c8856a03fe24c565105
mohammadadnangulzar-rgb <mohammadadnangulzar@gmail.com>
2026-05-16 01:17:52 +0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -136,7 +136,7 @@ def create_subtitles(VOICE_FILE):
                 color='white',
                 stroke_color='black',
                 stroke_width=4,
-                font="DejaVu-Sans", 
+                font="DejaVuSans", 
                 size=(subtitle_width, None),
                 bg_color=(0, 0, 0, 160),
                 method='caption'
